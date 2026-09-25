45849ea3bc1d8f11aeec2b6a9e7df545876d15cc
pavanchand5521 <pavanchand5521@gmail.com>
2026-09-25 22:49:23 +0530

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int asteroid:asteroids){
+
+            boolean alive = true;
+
+            while(alive && asteroid<0 && !stack.isEmpty() && stack.peek()>0){
+
+                int top = stack.peek();
+
+                if(top<Math.abs(asteroid)){
+                    stack.pop();
+                }else if(top == Math.abs(asteroid)){
+                    stack.pop();
+                    alive =false;
+                }else{
+                    alive = false;
+                }
+            }
+
+            if(alive){
+                stack.push(asteroid);
+            }
+        }
+
+        int result[] = new int[stack.size()];
+
+        for(int i=0;i<stack.size();i++){
+            result[i] = stack.get(i);
+        }
+       return result;
+    }
+}
